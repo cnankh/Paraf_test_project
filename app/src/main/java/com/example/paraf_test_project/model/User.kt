30fd898ac6203aec4b95bbcc240c8d1e405f747d
Sina <36819672+cnankh@users.noreply.github.com>
2021-08-30 18:20:37 +0430

    feat: gets user address
    user current address is given based on latitude and longitude

---
+++ app/src/main/java/com/example/paraf_test_project/model/User.kt
@@ -0,0 +1,5 @@
+package com.example.paraf_test_project.model
+
+data class User(
+    var currentAddress: String? = null
+)
---
+++ app/src/main/java/com/example/paraf_test_project/viewmodel/UserViewModel.kt
@@ -0,0 +1,37 @@
+package com.example.paraf_test_project.viewmodel
+
+import android.annotation.SuppressLint
+import android.app.Application
+import android.location.Geocoder
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import com.example.paraf_test_project.model.User
+import kotlinx.coroutines.launch
+import java.util.*
+
+class UserViewModel(application: Application) : BaseViewModel(application) {
+    private final val TAG = "tag-userViewModel: "
+
+    @SuppressLint("StaticFieldLeak")
+    private val context = getApplication<Application>().applicationContext
+    private lateinit var geocoder: Geocoder
+    val user = MutableLiveData<User>()
+
+    /**
+     * based on user latitude and longitude , get the address
+     */
+    fun getAddress(latitude: Double, longitude: Double) {
+        launch {
+            geocoder = Geocoder(context, Locale.getDefault())
+            val addresses = geocoder.getFromLocation(latitude, longitude, 1)
+            val address = addresses[0].getAddressLine(0)
+
+            val _user = User(address)
+
+            Log.d(TAG, addresses.toString())
+            user.value = _user
+        }
+
+    }
+
+}
